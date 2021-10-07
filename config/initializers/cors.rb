d00d4aec58ac744dc72798e94ce3478a501193eb
Jexica Ayran <jexica@jexicas-air.myfiosgateway.com>
2021-10-07 15:03:04 -0400

    updated deployed link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://digialtar.netlify.app/'
     # ^ we will replace * with the deployed link
 
     resource '*',
