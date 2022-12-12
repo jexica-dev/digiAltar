f48c58a6a733eea5d50b23550f8ecbb0fbdcf289
Jexica Ayran <jexica@macbook-air.myfiosgateway.com>
2022-12-11 21:18:50 -0500

    deployment test

--- client/src/services/api_config.js
+++ client/src/services/api_config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // the link heroku deployed link
 
-const baseURL = 'https://digialtar-api.herokuapp.com/';
+const baseURL = "https://digialtar-api.herokuapp.com/";
 
 const api = axios.create({
   baseURL: baseURL,
